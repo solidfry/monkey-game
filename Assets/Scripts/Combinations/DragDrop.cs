b460f05ff388e40cc4df6eab0ec9ec424111f554
Elliot <ellioteweb@gmail.com>
2023-12-04 16:15:35 +1100

    Cute drag drop anims

--- Assets/Scripts/Combinations/DragDrop.cs
+++ Assets/Scripts/Combinations/DragDrop.cs
@@ -1,7 +1,7 @@
 using Database;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using DG.Tweening;
 using UnityEngine;
+using Sequence = DG.Tweening.Sequence;
 
 public class DragDrop : MonoBehaviour
 {
@@ -10,15 +10,49 @@ public class DragDrop : MonoBehaviour
 
     [SerializeField] private CombinationDatabase combinationDB;
 
+   
+
+    [SerializeField] private Ease animationDragEase = Ease.InOutSine;
+    [SerializeField] private Ease animationDropEase = Ease.OutBounce;
+
+    [SerializeField]
+    float scaleOnDrag = 1.2f;
+    [SerializeField]
+    float scaleOnDrop = 1f;
+    [SerializeField]
+    float scaleDuration = 0.1f;
+
+    [Header("Drop Punch")] 
+    [SerializeField] private float punchDuration = .2f;
+    [SerializeField] private int punchVibrato = 2;
+    [SerializeField] private float punchElasticity = 4f;
+    [SerializeField] Vector2 punch = new Vector2(.1f, .3f);
+    
+    private TweenParams dragParams => new TweenParams().SetEase(animationDragEase);
+    private TweenParams dropParams => new TweenParams().SetEase(animationDropEase);
+
+    private Sequence Drag => DOTween.Sequence()
+        .Append(transform.DOScale(scaleOnDrag, scaleDuration).SetAs(dragParams).SetAutoKill(false));
+    private Sequence Drop => DOTween.Sequence()
+        .Append(transform.DOScale(scaleOnDrop, scaleDuration)
+            .SetAs(dropParams))
+        .AppendCallback(() =>
+        {
+            transform.DOPunchScale(punch, punchDuration, punchVibrato, punchElasticity);
+        }).SetAutoKill(false); 
+
     void Start()
     {
         animalRB = GetComponent<Rigidbody2D>();
         animalRB.bodyType = RigidbodyType2D.Kinematic;
+       
     }
 
     void OnMouseDown()
     {
         offset = (Vector2)transform.position - (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Drop.Rewind();
+        Drag.Play();
     }
 
     void OnMouseDrag()
@@ -31,8 +65,10 @@ public class DragDrop : MonoBehaviour
     {
         // Check for overlap after releasing the object
         CheckOverlap();
+        Drop.Play();
+        Drag.Rewind();
     }
-
+    
     void CheckOverlap()
     {
         Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0f);
